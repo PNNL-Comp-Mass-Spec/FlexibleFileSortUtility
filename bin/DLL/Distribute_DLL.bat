c785e7c4886da329389918285fddaffc29d0d254
alchemistmatt <matthew.monroe@pnnl.gov>
2018-10-24 18:35:13 -0700

    Add batch file

---
+++ bin/DLL/Distribute_DLL.bat
@@ -0,0 +1,13 @@
+@echo off
+
+xcopy FlexibleFileSortUtility.dll "F:\Documents\Projects\DataMining\Protein_Digestion_Simulator\bin\" /D /Y
+xcopy FlexibleFileSortUtility.dll "F:\Documents\Projects\DataMining\Protein_Digestion_Simulator\bin\DLL\" /D /Y
+xcopy FlexibleFileSortUtility.dll "F:\Documents\Projects\DataMining\Protein_Digestion_Simulator\Lib\" /D /Y
+
+xcopy FlexibleFileSortUtility.dll "F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Executables\Debug\" /D /Y
+xcopy FlexibleFileSortUtility.dll "F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Executables\Release\" /D /Y
+xcopy FlexibleFileSortUtility.dll "F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Protein_Uploader\bin\" /D /Y
+xcopy FlexibleFileSortUtility.dll "F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Shared Libraries\" /D /Y
+xcopy FlexibleFileSortUtility.dll "F:\Documents\Projects\DataMining\Validate_Fasta_File\TestValidateFastaFileDLL\bin\" /D /Y
+
+pause
