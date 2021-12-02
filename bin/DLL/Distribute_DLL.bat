130da75c22cc20a6e75c89b2e0406032124774eb
alchemistmatt <matthew.monroe@pnnl.gov>
2021-12-01 18:14:12 -0800

    Update distribution batch file

--- bin/DLL/Distribute_DLL.bat
+++ bin/DLL/Distribute_DLL.bat
@@ -1,7 +1,7 @@
 @echo off
 
-xcopy FlexibleFileSortUtility.dll F:\Documents\Projects\DataMining\Protein_Digestion_Simulator\bin\ /D /Y
-xcopy FlexibleFileSortUtility.dll F:\Documents\Projects\DataMining\Protein_Digestion_Simulator\bin\DLL\ /D /Y
+xcopy FlexibleFileSortUtility.dll F:\Documents\Projects\DataMining\Protein_Digestion_Simulator\ProteinDigestionSimulator\bin\ /D /Y
+xcopy FlexibleFileSortUtility.dll F:\Documents\Projects\DataMining\Protein_Digestion_Simulator\ProteinDigestionSimulator\bin\DLL\ /D /Y
 xcopy FlexibleFileSortUtility.dll F:\Documents\Projects\DataMining\Protein_Digestion_Simulator\Lib\ /D /Y
 
 xcopy FlexibleFileSortUtility.dll F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Executables\Debug\ /D /Y
@@ -10,9 +10,10 @@ xcopy FlexibleFileSortUtility.dll F:\Documents\Projects\KenAuberry\Organism_Data
 xcopy FlexibleFileSortUtility.dll F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Lib\ /D /Y
 xcopy FlexibleFileSortUtility.dll F:\Documents\Projects\DataMining\Validate_Fasta_File\TestValidateFastaFileDLL\bin\ /D /Y
 
-xcopy FlexibleFileSortUtility.dll F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common /D /Y
+xcopy FlexibleFileSortUtility.dll F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\ /D /Y
 
-xcopy FlexibleFileSortUtility.dll F:\Documents\Projects\DataMining\Validate_Fasta_File\Lib /D /Y
-xcopy FlexibleFileSortUtility.dll F:\Documents\Projects\DataMining\Validate_Fasta_File\bin /D /Y
+xcopy FlexibleFileSortUtility.dll F:\Documents\Projects\DataMining\Validate_Fasta_File\Lib\ /D /Y
+xcopy FlexibleFileSortUtility.dll F:\Documents\Projects\DataMining\Validate_Fasta_File\bin\ /D /Y
+xcopy FlexibleFileSortUtility.dll F:\Documents\Projects\DataMining\Validate_Fasta_File\bin\DLL\ /D /Y
 
 pause
